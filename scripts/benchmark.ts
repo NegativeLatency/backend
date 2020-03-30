14599743f5782c526578a0f40242963888be1afd
WenheLI <wl1508@nyu.edu>
2020-03-30 17:44:15 +0800

    add frame monitor

--- scripts/benchmark.ts
+++ scripts/benchmark.ts
@@ -1,6 +1,7 @@
 import { execFile } from "child_process"
 import * as RTMP from "../src/servers/rtmp-server"
 import * as fs from "fs"
+import { FramesMonitor } from 'video-quality-tools'
 
 const sleep = (it: number) => {
     return new Promise((resolve) => {
@@ -35,20 +36,61 @@ async function getMedia(name: string, url: string) {
 }
 
 const startServer = async () => {
-    RTMP.defaults().run();
-    await sleep(3000);
+    const instance = RTMP.defaults()
+    instance.run();
+    sleep(1000)
+    return instance;
 }
 
 const startStream = async (fileName: string) => {
-    await shellAsync(`ffmpeg -re -i ${fileName} -c copy -f flv rtmp://localhost/live/test`)
+    const url = "rtmp://localhost/live/test";
+    shellAsync(`ffmpeg -re -i ${fileName} -c copy -f flv ${url}`);
+    await sleep(100);
+    return url;
+}
+
+const reciveStream = async (streamUrl: string) => {
+    await shellAsync(`ffmpeg -i ${streamUrl} -acodec copy -vcodec copy dump.mp4`)
 }
 
 const main = async () => {
+
+    const PAUSE = 100;
+
     const fileName = await getMedia("test.mp4", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4");
-    await startServer();
-    await startStream(fileName);
+    const nms = await startServer();
 
+    const startTime = Date.now();
 
+    const streamUrl = await startStream(fileName);
+    const framesMonitorOptions = {
+        ffprobePath: '/usr/local/bin/ffprobe',
+        timeoutInMs: 2000,
+        bufferMaxLengthInBytes: 100000,
+        errorLevel: 'error',
+        exitProcessGuardTimeoutInMs: 1000,
+        analyzeDurationInMs: 9000
+    };
+    const framesMonitor = new FramesMonitor(framesMonitorOptions, streamUrl);
+ 
+    const videoLatency = []
+    const audioLatency = []
+
+    framesMonitor.on('frame', frameInfo => {
+        if (frameInfo.media_type == 'audio') {
+            audioLatency.push(frameInfo.pkt_pts_time * 1000 - PAUSE)
+        } else {
+            console.log(Date.now() - startTime)
+            videoLatency.push(frameInfo.pkt_pts_time * 1000 - PAUSE)
+        }
+    });
+
+    framesMonitor.on('exit', () => {
+        console.log(videoLatency);
+        nms.server.stop();
+    });
+     
+    framesMonitor.listen();
 }
 
 main();
